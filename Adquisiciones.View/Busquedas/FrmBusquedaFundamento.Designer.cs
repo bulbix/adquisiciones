f42c78f42c25281cf0fcd3846ba744231c633896
bulbix <mr.bulbo@gmail.com>
2013-07-08 17:08:00 -0500

    Cambios Menores

--- Adquisiciones.View/Busquedas/FrmBusquedaFundamento.Designer.cs
+++ Adquisiciones.View/Busquedas/FrmBusquedaFundamento.Designer.cs
@@ -32,6 +32,9 @@
             this.gvFundamento = new DevExpress.XtraGrid.Views.Grid.GridView();
             this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
             this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn3 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn4 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn5 = new DevExpress.XtraGrid.Columns.GridColumn();
             ((System.ComponentModel.ISupportInitialize)(this.bsSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gcFundamento)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gvFundamento)).BeginInit();
@@ -41,10 +44,10 @@
             // 
             this.gcFundamento.DataSource = this.bsSource;
             this.gcFundamento.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gcFundamento.Location = new System.Drawing.Point(0, 67);
+            this.gcFundamento.Location = new System.Drawing.Point(0, 69);
             this.gcFundamento.MainView = this.gvFundamento;
             this.gcFundamento.Name = "gcFundamento";
-            this.gcFundamento.Size = new System.Drawing.Size(784, 451);
+            this.gcFundamento.Size = new System.Drawing.Size(784, 448);
             this.gcFundamento.TabIndex = 5;
             this.gcFundamento.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gvFundamento});
@@ -53,6 +56,9 @@
             // 
             this.gvFundamento.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
             this.gridColumn2,
+            this.gridColumn3,
+            this.gridColumn4,
+            this.gridColumn5,
             this.gridColumn1});
             this.gvFundamento.GridControl = this.gcFundamento;
             this.gvFundamento.Name = "gvFundamento";
@@ -70,7 +76,7 @@
             this.gridColumn2.UnboundType = DevExpress.Data.UnboundColumnType.Integer;
             this.gridColumn2.Visible = true;
             this.gridColumn2.VisibleIndex = 0;
-            this.gridColumn2.Width = 104;
+            this.gridColumn2.Width = 58;
             // 
             // gridColumn1
             // 
@@ -79,8 +85,37 @@
             this.gridColumn1.Name = "gridColumn1";
             this.gridColumn1.UnboundType = DevExpress.Data.UnboundColumnType.String;
             this.gridColumn1.Visible = true;
-            this.gridColumn1.VisibleIndex = 1;
-            this.gridColumn1.Width = 680;
+            this.gridColumn1.VisibleIndex = 4;
+            this.gridColumn1.Width = 587;
+            // 
+            // gridColumn3
+            // 
+            this.gridColumn3.Caption = "Articulo";
+            this.gridColumn3.FieldName = "ArtiFundamento";
+            this.gridColumn3.Name = "gridColumn3";
+            this.gridColumn3.UnboundType = DevExpress.Data.UnboundColumnType.String;
+            this.gridColumn3.Visible = true;
+            this.gridColumn3.VisibleIndex = 1;
+            this.gridColumn3.Width = 62;
+            // 
+            // gridColumn4
+            // 
+            this.gridColumn4.Caption = "Inciso";
+            this.gridColumn4.FieldName = "IncisoFundamento";
+            this.gridColumn4.Name = "gridColumn4";
+            this.gridColumn4.UnboundType = DevExpress.Data.UnboundColumnType.String;
+            this.gridColumn4.Visible = true;
+            this.gridColumn4.VisibleIndex = 2;
+            this.gridColumn4.Width = 73;
+            // 
+            // gridColumn5
+            // 
+            this.gridColumn5.Caption = "Fraccion";
+            this.gridColumn5.FieldName = "FraccFundamento";
+            this.gridColumn5.Name = "gridColumn5";
+            this.gridColumn5.UnboundType = DevExpress.Data.UnboundColumnType.String;
+            this.gridColumn5.Visible = true;
+            this.gridColumn5.VisibleIndex = 3;
             // 
             // FrmBusquedaFundamento
             // 
@@ -104,5 +139,8 @@
         private DevExpress.XtraGrid.Views.Grid.GridView gvFundamento;
         private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
         private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn3;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn4;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn5;
     }
 }
